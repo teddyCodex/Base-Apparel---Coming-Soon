e4db6d9d46e130f3d1ae07cb53d5814227d13d4a
Teddy <samted.uche@gmail.com>
2022-07-11 18:12:57 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let errorMsg = document.getElementById("error-msg");
+let inputEl = document.getElementById("name");
+
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    document.form1.text1.focus();
+    return true;
+  } else {
+    errorMsg.textContent = "Please provide a valid email";
+    inputEl.classList.add("active");
+    document.form1.text1.focus();
+    return false;
+  }
+}
+
+document.addEventListener(
+  "invalid",
+  (function () {
+    return function (e) {
+      e.preventDefault();
+      document.getElementById("name").focus();
+    };
+  })(),
+  true
+);
