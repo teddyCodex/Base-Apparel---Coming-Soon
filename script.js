a4426299bb3192fad4f753aca93eaf0c8347a2db
Teddy <samted.uche@gmail.com>
2022-07-12 14:15:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let errorMsg = document.getElementById("error-msg");
 let inputEl = document.getElementById("name");
+let svgEl = document.getElementById("hidden");
 
 function ValidateEmail(inputText) {
   var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -9,6 +10,7 @@ function ValidateEmail(inputText) {
   } else {
     errorMsg.textContent = "Please provide a valid email";
     inputEl.classList.add("active");
+    svgEl.classList.remove("hidden");
     document.form1.text1.focus();
     return false;
   }
